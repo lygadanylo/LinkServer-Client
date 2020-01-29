c26e61d6a6a5f806142d7a6e7fcdd9bdfc677e83
Danylo Lyha <danylo.lyha@synapse.com>
2020-01-29 16:30:08 +0200

    protected routs

--- client/src/components/upload.js
+++ client/src/components/upload.js
@@ -3,11 +3,24 @@ import { connect } from 'react-redux';
 import { loadFiles } from '../common/action';
 
 class Upload extends Component {
+	componentDidMount() {
+		const token = localStorage.getItem('token');
+		if (!token) {
+			return this.logout();
+		}
+	}
+
+	logout = () => {
+		localStorage.clear();
+		this.props.history.push('/');
+	};
+
 	render() {
 		const { loadFiles } = this.props;
 		return (
 			<div>
 				<button onClick={() => loadFiles()}>Download files</button>
+				<button onClick={() => this.logout()}>Log Out</button>
 			</div>
 		);
 	}
